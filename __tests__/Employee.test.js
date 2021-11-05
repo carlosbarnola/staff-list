e25bb91d935907dfdf1e79b4298de36e7c3c617a
Carlos Rojas <carlosrb206@gmail.com>
2021-11-04 23:28:06 -0400

    test code

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,25 @@
+// const Employee = require('../lib/Employee');
+
+// test('creates an Employee object', () => {
+//     const employee = new Employee('Jim');
+
+//     expect(employee.name).toBe('Jim');
+//     expect(employee.id).toEqual(expect.any(Number));
+//     expect(employee.email).toEqual(expect.any(String));
+    
+// });
+
+// test('gets employee name as an string', () => {
+//     const employee = new Employee('Jim');
+
+//     expect(employee.getName()).toBe('Jim');
+// })
+
+// test('gets id as a number', () => {
+//     const employee = new Employee('Jim');
+//     const oldID = employee.id;
+
+//     employee.getId(2);
+    
+//     expect(employee.id).toBe(oldID ++);
+// })
