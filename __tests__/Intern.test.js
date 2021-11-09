f5cb18424262ab863242af2572999b4e607f0e59
Carlos Rojas <carlosrb206@gmail.com>
2021-11-08 20:41:15 -0500

    intern code and test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -8,3 +8,15 @@ test('creates an Intern object', () => {
     expect(intern.email).toEqual(expect.any(String));
     expect(intern.school).toEqual(expect.any(String));    
 });
+
+test('gets Intern school name as a string', () => {
+    const intern = new Intern('Jim', 32, 'jim@gmail.com', 'GW University');
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test('gets Intern role as an string', () => {
+    const intern = new Intern('Jim', 32, 'jim@gmail.com', 'GW University');
+
+    expect(intern.getRole()).toEqual('Intern');
+});
